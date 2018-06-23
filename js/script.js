6bab13b78b110e5b0a286fc50405bb6c48924e27
Klukluklukli <klukluklukli@hotmail.com>
2018-06-23 09:31:57 -0500

    Git init

---
+++ js/script.js
@@ -0,0 +1,15 @@
+/*
+ Autor Klukluklukli
+*/
+
+var inputNombre = document.getElementById('nombre');
+var inputApellido = document.getElementById('apellido');
+var inputPassword = document.getElementById('password');
+
+var boton = document.getElementById('button');
+
+boton.addEventListener('click', handle);
+
+function handle(e) {
+  console.log(inputNombre.value);
+}
